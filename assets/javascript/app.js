52ddcd0547d13930f73f78cc36b45578f62f0ec2
Pachosalazar <jfsh87@hotmail.com>
2018-03-21 17:45:41 -0400

    something more

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,29 +2,36 @@
 
 var start;
 var HTMLDOM;
-var time = 30;
-var question = ["What is the name of the Simpsons next door neighbour?",
-                "In which town do the Simpson's reside?", 
-                "What did Bart name his pet elephant?",
-                "What is the name of the school bus driver?", 
-               ];
-var answer = [["Ned_Flanders"," Otto", "Homer", "Apu"],
-              ["Springfield", "Demopolis","Birmingham", "Guntersville"],
-              ["Stampy", "Stoner: 1","Monty:2"," Rosty: 3"],
-              ["Otto", "Timothy","Lyle","Apu"]
-                ];
-var rightanswer = ["Ned Flanders", "Springfield", "Stampy", "Otto"];
+var time = 0;
+
+// add another array within the array to use the index of the first one
+var question = [
+    ["What is the name of the Simpsons next door neighbour?"],
+    ["In which town do the Simpson's reside?"],
+    ["What did Bart name his pet elephant?"],
+    ["What is the name of the school bus driver?"],
+];
+// change answers to arrays to
+// answer[0] = [ "Ned Flanders", " Otto", "Homer", "Apu" ]
+//  answers[0][0] = Ned Flanders
+var answer = [
+    ["Ned Flanders", " Otto", "Homer", "Apu"],
+    ["Springfield", "Demopolis", "Birmingham", "Guntersville"],
+    ["Stampy", "Stoner: 1", "Monty:2", " Rosty: 3"],
+    ["Otto", "Timothy", "Lyle", "Apu"]
+];
+var right = ["A. Ned Flanders", "A. Springfield", "A. Stampy", "A. Otto"];
+// right[0][0] = "Ned Flanders";
 var Counter = 0;
 var crono;
-var right = 0;
 var Wrong = 0;
-var blank= 0;
+var blank = 0;
 var music = new Audio("assets/sound/The-Simpsons-Theme-Song.mp3");
 var good = new Audio("assets/sound/Bart - Cool Man.wav");
 var bad = new Audio("assets/sound/Homer - D'oh! Wrong one!.wav");
 
-$(document).ready(function () {
- 
+$(document).ready(function() {
+
     function initialScreen() {
         startScreen = "<p class='text-center main-button-container'><a class='btn btn-danger btn-lg btn-block start' href='#' role='button'>Are you ready!!</a></p>";
         $("#wrapper").html(startScreen);
@@ -33,19 +40,22 @@ $(document).ready(function () {
     initialScreen();
 
 
-    $("body").on("click", ".start", function (event) {
-        event.preventDefault(); 
+    $("body").on("click", ".start", function(event) {
+        event.preventDefault();
         music.play();
         generateHTML();
         timerframe();
 
     });
 
-    $("body").on("click", ".answer", function (event) {
-       
+    $("body").on("click", ".answer", function(event) {
+
         selectedAnswer = $(this).text();
-        if (selectedAnswer === rightanswer[Counter]) {
-           
+
+        console.log("selectedAnswer", selectedAnswer)
+        console.log("right[Counter]", right[Counter])
+
+        if (selectedAnswer === right[Counter]) {
 
             clearInterval(crono);
             good.play();
@@ -55,57 +65,57 @@ $(document).ready(function () {
             bad.play();
             Lost();
         }
-    }); 
+    });
 
-    $("body").on("click", ".reset-button", function (event) {
+    $("body").on("click", ".reset-button", function(event) {
         music.play();
         resetHTMLDOM();
-    }); 
+    });
 
-}); 
+});
 
 function LossTimeOut() {
     blank++;
-    HTMLDOM= "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" 
-    + time + "</span></p>" + "<p class='text-center'>Bad luck man no more time " 
-    + rightanswer[Counter] + "</p>" 
-    + "<img class='center-block img-wrong' src='image/timeout-homer.gif'>"
+    HTMLDOM = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" +
+        Counter + "</span></p>" + "<p class='text-center'>Bad luck man no more time " +
+        right[Counter] + "</p>" +
+        "<img class='center-block img-wrong' src='../image/timeout homer.gif'>";
     $("#wrapper").html(HTMLDOM);
     setTimeout(timer, 4000);
 }
 
 function Win() {
     right++;
-    HTMLDOM= "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" 
-    + time + "</span></p>" + "<p class='text-center'>Correct! The answer is: " 
-    + rightanswer[Counter] + "</p>" + "<img class='center-block img-right' src='image/bart.gif'>";
+    HTMLDOM = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" +
+        Counter + "</span></p>" + "<p class='text-center'>Correct! The answer is: " +
+        right[Counter] + "</p>" + "<img class='center-block img-right' src='../image/bart.gif'>";
     $("#wrapper").html(HTMLDOM);
-    setTimeout(timer, 4000); 
+    setTimeout(timer, 4000);
 }
 
 function Lost() {
     Wrong++;
-    HTMLDOM = "<p class='text-center timer-p'>Tick Tack tick tack <span class='timer'>" 
-    + time + "</span></p>" + "<p class='text-center'>Wrong! Wrong! Wrong! "
-     + rightanswer[Counter] + "</p>" 
-     + "<img class='center-block img-wrong' src='image/timeout-homer.gif'>";
+    HTMLDOM = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" +
+        Counter + "</span></p>" + "<p class='text-center'>Wrong! Wrong! Wrong! " +
+        right[Counter] + "</p>" +
+        "<img class='center-block img-wrong' src='../image/homer.gif'>";
     $("#wrapper").html(HTMLDOM);
-    setTimeout(timer, 4000); 
+    setTimeout(timer, 4000);
 }
 
 function generateHTML() {
-    HTMLDOM = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>"
-     + question[Counter] + "</p><p class='first-answer answer'>A. " + answer[Counter][0] + 
-     "</p><p class='answer'>B. " + answer[Counter][1] + "</p><p class='answer'>C. " 
-     + answer[Counter][2] + "</p><p class='answer'>D. " + answer[Counter][3] + "</p>";
+    HTMLDOM = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>" +
+        question[Counter] + "</p><p class='first-answer answer'>A. " + answer[Counter][0] +
+        "</p><p class='answer'>B. " + answer[Counter][1] + "</p><p class='answer'>C. " +
+        answer[Counter][2] + "</p><p class='answer'>D. " + answer[Counter][3] + "</p>";
     $("#wrapper").html(HTMLDOM);
 }
 
 function timer() {
-    if (Counter < 4) {
-        Counter++;
+    if (time < 4) {
+        time++;
         generateHTML();
-        time = 0;
+        Counter = 30;
         timerframe();
     } else {
         finalScreen();
@@ -116,33 +126,32 @@ function timerframe() {
     crono = setInterval(thirtySeconds, 1000);
 
     function thirtySeconds() {
-        if (time === 0) {
+        if (Counter === 0) {
             clearInterval(crono);
             LossTimeOut();
         }
-        if (time > 0) {
-            time--;
+        if (Counter > 0) {
+            Counter--;
         }
-        $(".timer").html(time);
+        $(".timer").html(Counter);
     }
 }
 
 function finalScreen() {
-    HTMLDOM = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" 
-    + time + "</span></p>" + "<p class='text-center'>Finally Yeah!!"
-    + "</p>" + "<p class='summary-correct'> " + right + "</p>" 
-    + "<p>Wrong Answers: " + Wrong + "</p>" + "<p>Missed " + blank + "</p>"
-    + "<p class='text-center reset-button-container'><a class='btn btn-danger btn-lg btn-block reset-button' href='#' role='button'>U wanna play again?</a></p>";
+    HTMLDOM = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" +
+        Counter + "</span></p>" + "<p class='text-center'>Finally Yeah!!" +
+        "</p>" + "<p class='summary-correct'> " + right + "</p>" +
+        "<p>Wrong Answers: " + Wrong + "</p>" + "<p>Missed " + blank + "</p>" +
+        "<p class='text-center reset-button-container'><a class='btn btn-danger btn-lg btn-block reset-button' href='#' role='button'>U wanna play again?</a></p>";
     $("#wrapper").html(HTMLDOM);
 }
 
 function resetHTMLDOM() {
-   time = 30;
+    time = 0;
     right = 0;
     Wrong = 0;
     blank = 0;
-    Counter = 0;
+    Counter = 30;
     generateHTML();
     timerframe();
 }
-
